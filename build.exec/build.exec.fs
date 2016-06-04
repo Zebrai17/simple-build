e94d1b345490fa573a0286acf1990d444680c760
root <rick.doucet@implementingeventsourcingwithfsharp.com>
2016-06-04 09:56:07 -0400

    Created the building blocks for the Command Factory

--- build.exec/build.exec.fs
+++ build.exec/build.exec.fs
@@ -6,7 +6,18 @@ let load = Seq.empty<BuildProcess.Event>
 let state = load |> Seq.fold BuildProcess.apply (BuildProcess.State.Zero())
 
 
+let printUsageInstructions = 
+    sprintf "%s" "usage {0} <command>", System.AppDomain.CurrentDomain.FriendlyName
+
+
+
 [<EntryPoint>]
 let main argv =
-    printfn "%A" argv
+    let commandResult = CommandFactory.create argv
+    
+    match commandResult with
+    | CommandFactory.Error e when e = CommandFactory.CommandNotProvided -> printUsageInstructions |> ignore
+    | _ -> sprintf "%s" "Error" |> ignore
+    
+    
     0 // return an integer exit code
